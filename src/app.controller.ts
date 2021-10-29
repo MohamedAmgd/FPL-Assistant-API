f1aad2f095a0476a53f7319e7b51cbf952be4f30
Mohamed Amgd <mohamedamgd97@gmail.com>
2021-10-30 00:10:19 +0200

    added cost_change_prediction endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -32,4 +32,18 @@ export class AppController {
     }
     return this.appService.getPlayerCostChange(filter_options);
   }
+
+  @Get("cost_change_prediction")
+  getPlayerCostChangePrediction(@Query("min_price") min_price: number,
+    @Query("max_price") max_price: number) {
+
+    let filter_options = new Map();
+    if (min_price) {
+      filter_options.set("min_price", min_price);
+    }
+    if (max_price) {
+      filter_options.set("max_price", max_price);
+    }
+    return this.appService.getPlayerCostChangePrediction(filter_options);
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
+import * as cheerio from 'cheerio';
 
 @Injectable()
 export class AppService {
@@ -56,6 +57,67 @@ export class AppService {
       return b.cost_change_event - a.cost_change_event;
     });
     return players;
+  }
+
+  async getPlayerCostChangePrediction(filter_options: any): Promise<any> {
+    let secrectParam = await this.getSecretParam();
+    const headersRequest = {
+      'Referer': 'http://www.fplstatistics.co.uk/Home/IndexAndroid2',
+      'X-Requested-With': 'XMLHttpRequest'
+    };
+    const result = await firstValueFrom(this.httpService
+      .get(`http://www.fplstatistics.co.uk/Home/AjaxPricesIHandler?${secrectParam.paramName}=${secrectParam.paramValue}&pyseltype=0&_=${Date.now()}`
+        , { headers: headersRequest }));
+    let players: any[] = [];
+    result.data.aaData.map(e => {
+      return {
+        first_name: '',
+        second_name: e[1],
+        team: e[2],
+        position: e[3],
+        now_cost: e[6] * 10,
+        delta: e[10],
+        target: e[11],
+        fixtures: e[16]
+      }
+    }).filter(e => !this.shouldBeFilltered(e, filter_options))
+      .forEach(element => {
+        players.push({
+          name: element.first_name + " " + element.second_name,
+          team: element.team,
+          position: element.position,
+          cost: element.now_cost / 10,
+          delta: element.delta,
+          target: element.target,
+          fixtures: element.fixtures
+        });
+      })
+    players.sort((a, b) => {
+      return b.target - a.target;
+    });
+    return players;
+  }
+  async getSecretParam() {
+    const homeHtml = await firstValueFrom(this.httpService
+      .get(`http://www.fplstatistics.co.uk/`));
+    const $ = cheerio.load(homeHtml.data);
+    let params = [];
+    $('script', homeHtml.data).each((i, e) => {
+      if (e.attribs.type === "text/javascript") {
+        let decodedScript = $(e.children).text();
+        let startIndexOfParamName = decodedScript.indexOf('\"\\x6E\\x61\\x6D\\x65\":\"') + '\"\\x6E\\x61\\x6D\\x65\":\"'.length;
+        let endIndexOfParamName = decodedScript.indexOf('\",\"', startIndexOfParamName);
+        let startIndexOfParamValue = decodedScript.indexOf('"\\x76\\x61\\x6C\\x75\\x65":') + '"\\x76\\x61\\x6C\\x75\\x65":'.length;
+        let endIndexOfParamValue = decodedScript.indexOf("});", startIndexOfParamValue);
+        let paramName = decodedScript.substring(startIndexOfParamName, endIndexOfParamName);
+        let paramValue = decodedScript.substring(startIndexOfParamValue, endIndexOfParamValue);
+        params.push({
+          paramName: paramName,
+          paramValue: paramValue
+        });
+      }
+    });
+    return params[0];
   }
   shouldBeFilltered(element: any, filter_options: Map<string, any>): boolean {
     let cost = element.now_cost / 10;
